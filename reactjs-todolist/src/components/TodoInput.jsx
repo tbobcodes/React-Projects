aaf4b883bf2c6588d618ce1158b7865bb46340e4
tbobcodes <jcoupet11@gmail.com>
2024-09-01 10:45:56 -0500

    huge commit holy, add all the todolist functionality and it can persist data through local storage

--- reactjs-todolist/src/components/TodoInput.jsx
+++ reactjs-todolist/src/components/TodoInput.jsx
@@ -1,7 +1,18 @@
-export default function TodoInput() {
+import { useState } from "react"
+
+export default function TodoInput(props) {
+
+  const { handleAddTodos, todoValue, setTodoValue } = props
+
   return (
-    <div>
-        TodoInput
-    </div>
+    <header>
+        <input value = {todoValue} onChange={(e) => {
+          setTodoValue(e.target.value)
+        }} placeholder="Enter todo..." />
+        <button onClick={() => {
+          handleAddTodos(todoValue)
+          setTodoValue('')
+        }}>Add</button>
+    </header>
   )
 }
